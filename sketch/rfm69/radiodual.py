e964eaeb8f820991c1ce4db12af4df992d95ccc3
blascarr <adrian.blascarr@gmail.com>
2025-04-20 16:52:17 +0200

    dual cansat

---
+++ sketch/rfm69/radiodual.py
@@ -0,0 +1,49 @@
+import subprocess
+import time
+import threading
+
+EXECUTABLE_PATH = "RadioHead/examples/raspi/rf69/rf69_client"
+mensaje_base = "TESTPYTHON"
+CLIENT_EXECUTABLE_PATH = "RadioHead/examples/raspi/rf69/rf69_client"
+SERVER_EXECUTABLE_PATH = "RadioHead/examples/raspi/rf69/rf69_server"
+base_message = "TESTPYTHON"
+
+counter = 0
+
+
+def send_messages():
+    try:
+        while True:
+
+            message = f"{base_message} {counter}"
+            print(f"Sending: {message}")
+            subprocess.run([CLIENT_EXECUTABLE_PATH, message])
+            counter += 1
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        print("\nSending program interrupted. Ending test.")
+
+def listen_messages():
+    try:
+        with subprocess.Popen(SERVER_EXECUTABLE_PATH, stdout=subprocess.PIPE, text=True) as proc:
+            while True:
+                output = proc.stdout.readline()
+                if output:
+                    print(f"Received: {output.strip()}")
+                else:
+                    break
+    except KeyboardInterrupt:
+        print("\nListening program interrupted. Ending test.")
+
+if __name__ == '__main__':
+    send_thread = threading.Thread(target=send_messages)
+    listen_thread = threading.Thread(target=listen_messages)
+
+    send_thread.start()
+    listen_thread.start()
+
+
+
+    send_thread.join()
+    listen_thread.join()
